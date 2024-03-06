3052dd6fd3956942b054ff1bedbfbb5fd760928c
papaskripto <bygeoffreyouma@gmail.com>
2024-03-06 13:17:08 +0300

    Add about-us view in app.py file

--- app/app.py
+++ app/app.py
@@ -8,3 +8,8 @@ app = Flask(__name__)
 def home():
     ''' Takes you home.'''
     return '<p>Hello, Cool person!</p>'
+
+@app.route('/about-us')
+def about():
+    ''' Find more about us.'''
+    return '<p>We are actually cooler than you think.</p>'
